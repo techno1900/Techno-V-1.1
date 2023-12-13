f6d6172c24128eab10fa2f6f00fa65cfabbf6e80
techno1900 <117414883+techno1900@users.noreply.github.com>
2023-12-13 15:19:44 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 pip install -r requirements.txt
-python3.9 manage.py collectstatic --noinput --clear
+python3.9 manage.py collectstatic --noinput
 
 python3 manage.py makemigrations --noinput
 python3 manage.py migrate --noinput
