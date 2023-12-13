f5016ae4735f8949c0229353fdb5d56c9653afb1
techno1900 <117414883+techno1900@users.noreply.github.com>
2023-12-13 16:02:18 +0530

    Update settings.py

--- TechnoBlog/settings.py
+++ TechnoBlog/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-603#6i@+r4hal9u-*(#-a^sx^_ok4qdq*_*-2!5!(eu=uwy!-i'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
