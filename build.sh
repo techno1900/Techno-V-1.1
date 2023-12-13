2a1d949316d3f77219266f19a8b0b520501864d9
techno1900 <117414883+techno1900@users.noreply.github.com>
2023-12-13 15:52:18 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,12 +1,8 @@
 
 # Build the project
 echo "Building the project..."
-python -m pip install -r requirements.txt
+pip install -r requirements.txt
 
-# Make migrations
-echo "Making migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
 
 # Collect static files
 echo "Collecting static files..."
