fe54aa3ba2d7e680cc4fef8fb2a3928496ebaeb8
techno1900 <117414883+techno1900@users.noreply.github.com>
2023-12-13 13:48:58 +0530

    new

--- TechnoBlog/settings.py
+++ TechnoBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-603#6i@+r4hal9u-*(#-a^sx^_ok4qdq*_*-2!5!(eu=uwy!-i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh', '127.0.0.1']
 
 
 # Application definition
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'TechnoBlog.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'sT5gtfslEgNQugq4',
+        'HOST': 'db.xcfpgyyojjwuazhyrene.supabase.co',
+        'PORT': '5432',
     }
 }
 
