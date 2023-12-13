9c5ea24eaf97bee043102535cf4a17259b29f1a0
techno1900 <117414883+techno1900@users.noreply.github.com>
2023-12-13 10:30:52 +0530

    a

--- TechnoBlog/settings.py
+++ TechnoBlog/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-603#6i@+r4hal9u-*(#-a^sx^_ok4qdq*_*-2!5!(eu=uwy!-i
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','.now.sh','127.0.0.1','localhost']
+ALLOWED_HOSTS = ['.vercel.app','.now.sh']
 
 
 # Application definition
