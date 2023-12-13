9d25dcef418b21195ca4d70b2a8e951a6e763a3c
techno1900 <117414883+techno1900@users.noreply.github.com>
2023-12-13 15:47:31 +0530

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,11 +1,7 @@
 # Create a virtual environment
 echo "Creating a virtual environment..."
-python3.9 -m env env
 source env/Script/activate
 
-# Install the latest version of pip
-echo "Installing the latest version of pip..."
-python -m pip install --upgrade pip
 
 # Build the project
 echo "Building the project..."
